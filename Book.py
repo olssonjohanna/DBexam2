ce92b709be16bf4987440a4585df6a181c9b8390
fadi123456 <fadiakrawi@gmail.com>
2017-11-28 14:09:57 +0100

    Book är KANSKE klar

--- Book.py
+++ Book.py
@@ -1,4 +1,4 @@
-
+import pyodbc
 class Book:
     def __init__(self,title,isbn,id,pages,author_name):
         self.title = title
@@ -19,18 +19,62 @@ class ManageBook:
         pass
 
     def getBookByID(self,id):
+        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=DBexam2; Trusted_Connection=yes")
+
         #get a book by id
         #return it as type of Book
         #if book oes not exist return None
-        pass
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM Book;")
+        row = cursor.fechone()
+        while row:
+            if len(row)==0:
+                break
+            if row[0] == id:
+                book = Book(row.title,row.isbn,row.id,row.pages,row.author_name)
+                break
+
+        cursor.close()
+        connection.commit()
+        connection.close()
+        return book
 
     def getBooksByIsbn(self,isbn):
         #bring books by isbn
         #return list
-        pass
+        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=DBexam2; Trusted_Connection=yes")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM Book;")
+        row = cursor.fechone()
+        while row:
+            if len(row) == 0:
+                break
+            if row == isbn:
+                book2=Book(row.title,row.isbn,row.id,row.pages,row.author_name)
+                break
 
+        cursor.close()
+        connection.commit()
+        connection.close()
+        return book2
     def getAllBooks(self):
+        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=DBexam2; Trusted_Connection=yes")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM Book; ")
+        row = cursor.fechone()
+        while row:
+            if len(row) == 0:
+                break
+            else:
+                self.list_of_Books = []
+                new_book = Book(row.title,row.isb,row.id,row.pages,row.author_name)
+                self.list_of_Books.append(new_book)
+
+        cursor.close()
+        connection.commit()
+        connection.close()
+        return self.list_of_Books
+
+
         #bring all book from db
         #return list
-        pass
-
