92f4ff911b9c00b1c99a38890c2737040db9a13c
fadi123456 <fadiakrawi@gmail.com>
2017-11-29 11:01:06 +0100

    Fixade connection i Useres

--- User.py
+++ User.py
@@ -8,25 +8,26 @@ class User:
         self.password = password
         self.list_of_booked_books = []
         self.list_of_borrowed_books = []
+        self.connection =pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
 
     def reserve(self,isbn):
         # work here on DB and list of booked
-        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
 
-        cursor = connection.cursor()
+
+        cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM Book where isbn = '" + str(isbn) + "';")
         data = cursor.fetchone
 
 
 
         cursor.close()
-        connection.commit()
-        connection.close()
+        self.connection.commit()
+        self.connection.close()
 
     def leaveBackBook(self,book_id):  #FUNKAR
         # the book will ne left back if it's really borrowed and to be sure of that you must see the db
-        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
-        cursor = connection.cursor()
+        #connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+        cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM Borrow WHERE BookID = '" + str(book_id) + "';")
         data = cursor.fetchone
 
@@ -42,8 +43,8 @@ class User:
 
 
         cursor.close()
-        connection.commit()
-        connection.close()
+        self.connection.commit()
+        self.connection.close()
 
     def toString(self):
         return_string = "|Name: " + str(self.name) + " |Email: " + str(self.email) + " |Address: " + str(self.address) + " |Password: " + str(self.password)
@@ -52,26 +53,28 @@ class User:
 class ManageUsers:
     def __init__(self):
         self.list_of_online_users = []
+        self.connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+
 
     def addUser(self,email,name,address,password): #FUNKAR men uppdateras inte i DB
         # check that same email does not exists and if exists return false
         # else add user to the db and return true
 
-        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+        #connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
         new_user = (name, email, address, password)
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         cursor.execute("INSERT INTO Users VALUES "+ str(new_user) + ";")
     #cursor.fetchone()
 
         return True
 
         cursor.close()
-        connection.commit()
-        connection.close()
+        self.connection.commit()
+        self.connection.close()
 
     def tryToLogIn(self,email,password): #FUNKAR!
-        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
-        cursor = connection.cursor()
+        #connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+        cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM Users WHERE UserEmail = '" + str(email) + "' AND Password = '"+str(password)+"';")
 
         data = cursor.fetchone()
@@ -85,12 +88,12 @@ class ManageUsers:
             return True
 
         cursor.close()
-        connection.commit()
-        connection.close()
+        self.connection.commit()
+        self.connection.close()
 
     def getAllUsers(self):  #FUNKAR
-        connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
-        cursor = connection.cursor()
+        #connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+        cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM Users;")
         data = cursor.fetchone()
 
@@ -109,22 +112,22 @@ class ManageUsers:
             return list
 
         cursor.close()
-        connection.commit()
-        connection.close()
+        self.connection.commit()
+        self.connection.close()
 
     def logOut(self,email): #INTE KLAR
         for i in range(len(self.list_of_online_users)):
             if self.list_of_online_users[i].email == email:
                 userToBeOutLogged = self.list_of_online_users.pop(i)
 
-                connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
+                #connection = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server}; Server=localhost; Database=Exam2; Trusted_Connection=yes")
 
-                cursor = connection.cursor()
+                cursor = self.connection.cursor()
                 cursor.execute("UPDATE .... ")
 
                 cursor.close()
-                connection.commit()
-                connection.close()
+                self.connection.commit()
+                self.connection.close()
 
                 #save changes of user to DB
                 break
